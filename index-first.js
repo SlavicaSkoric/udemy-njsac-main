52d1ddc5805354d45f5cdfd65c994d4363f1b34a
SlavicaSkoric <skoric.slavica.hr@gmail.com>
2022-03-10 15:48:16 +0100

    add all files

---
+++ index-first.js
@@ -0,0 +1,41 @@
+process.env.UV_THREADPOOL_SIZE = 1;
+const cluster = require('cluster');
+
+// console.log(cluster.isMaster);
+
+// Is the file being executed in master mode?
+if (cluster.isMaster) {
+  // Cause index.js to be executed *again* but in child mode (by a worker instance)
+  cluster.fork();
+  cluster.fork();
+  // cluster.fork();
+  // cluster.fork();
+  // cluster.fork();
+  // cluster.fork();
+} else {
+  // I'm a child, I'm going to act like a server and do nothing else
+  const express = require('express');
+  const crypto = require('crypto');
+  const app = express();
+
+  /*   function doWork(duration) {
+    const start = Date.now();
+    while (Date.now() - start < duration) {}
+  } */
+
+  app.get('/', (req, res) => {
+    // doWork(5000);
+    // simulate some work, some computation on our server:
+    // work on our hash:
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+      // console.log('1:', Date.now() - start, 'ms');
+      res.send('Hiii thereee');
+    });
+  });
+
+  app.get('/fast', (req, res) => {
+    res.send('This was fast!');
+  });
+
+  app.listen(3000);
+}
